a839acf57513faa5498601d3355853b7b2abf285
des-ant <57375593+des-ant@users.noreply.github.com>
2021-12-16 22:34:04 +1100

    Write a function calculateBmi that calculates a BMI based on a given height (in centimeters) and weight (in kilograms) and then returns a message that suits the results. -ex9.1

---
+++ part9b-exercises/bmiCalculator.ts
@@ -0,0 +1,19 @@
+type Result = string;
+
+const calculateBmi = (height: number, mass: number) : Result => {
+  const heightInM : number = height / 100;
+  const BMIFloat: number = mass / (heightInM ** 2);
+  const BMI: number = Math.round(BMIFloat * 10) / 10;
+  return (
+    BMI < 16.0 ? 'Underweight (Severe thinness)' :
+    BMI <= 16.9 ? 'Underweight (Moderate thinness)' :
+    BMI <= 18.4 ? 'Underweight (Mild thinness)' :
+    BMI <= 24.9 ? 'Normal range' :
+    BMI <= 29.9 ? 'Overweight (Pre-obese)' :
+    BMI <= 34.9 ? 'Obese (Class I)' :
+    BMI <- 39.9 ? 'Obese (Class III)' :
+    'Obese (Class III)'
+  );
+}
+
+console.log(calculateBmi(180, 74));
