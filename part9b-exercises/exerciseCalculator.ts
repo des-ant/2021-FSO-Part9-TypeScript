a93682545391d28f635926f0321c4d9f845860f2
des-ant <57375593+des-ant@users.noreply.github.com>
2021-12-19 11:05:54 +1100

    Change exerciseCalculator to work with command line arguments. -ex9.3

--- part9b-exercises/exerciseCalculator.ts
+++ part9b-exercises/exerciseCalculator.ts
@@ -1,4 +1,57 @@
-interface Result {
+/*
+Input: npm run calculateExercises 2 3 0 2 4.5 0 3 1
+Output:
+{ periodLength: 7,
+  trainingDays: 5,
+  success: false,
+  rating: 2,
+  ratingDescription: 'not too bad but could be better',
+  target: 2,
+  average: 1.9285714285714286 }
+
+Input: npm run calculateExercises 2 1 0 2 4.5 0 3 1 0 4
+Output:
+{ periodLength: 9,
+  trainingDays: 6,
+  success: false,
+  rating: 2,
+  ratingDescription: 'not too bad but could be better',
+  target: 2,
+  average: 1.7222222222222223 }
+*/
+
+interface exerciseValues {
+  target: number;
+  dailyExerciseHours: Array<number>;
+}
+
+const parseArgumentsExercise = (args: Array<string>): exerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  // Ignore first two arguments that are required for the npm script to run this
+  // program
+  const [, , targetInput, ...inputList] = args;
+
+  if(isNaN(Number(targetInput))) {
+    throw new Error('Provided values were not numbers!');
+  }
+
+  const target: number = Number(targetInput);
+
+  const dailyExerciseHours: Array<number> = inputList.map(val => {
+    if(isNaN(Number(val))) {
+      throw new Error('Provided values were not numbers!');
+    }
+    return Number(val);
+  })
+
+  return {
+    target,
+    dailyExerciseHours
+  }
+}
+
+interface exerciseResult {
   periodLength: number;
   trainingDays: number;
   success: boolean;
@@ -8,7 +61,7 @@ interface Result {
   average: number;
 }
 
-const calculateExercises = (dailyExerciseHours: Array<number>): Result => {
+const calculateExercises = (target: number, dailyExerciseHours: Array<number>): exerciseResult => {
   const periodLength: number = dailyExerciseHours.length;
   const trainingDays: number = dailyExerciseHours
     .filter(dailyHour => dailyHour > 0)
@@ -16,7 +69,6 @@ const calculateExercises = (dailyExerciseHours: Array<number>): Result => {
   const totalHours: number = dailyExerciseHours
     .reduce((a, b) => a + b, 0)
   const average: number = totalHours / periodLength;
-  const target: number = 2;
   const success: boolean = average >= target;
   const rating: number =
     average / target >= 1 ? 3 :
@@ -38,13 +90,14 @@ const calculateExercises = (dailyExerciseHours: Array<number>): Result => {
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]));
-/*
-{ periodLength: 7,
-  trainingDays: 5,
-  success: false,
-  rating: 2,
-  ratingDescription: 'not too bad but could be better',
-  target: 2,
-  average: 1.9285714285714286 }
-*/
+
+try {
+  const { target, dailyExerciseHours } = parseArgumentsExercise(process.argv);
+  console.log(calculateExercises(target, dailyExerciseHours));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
