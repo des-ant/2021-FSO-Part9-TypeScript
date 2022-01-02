77055ea0fc3c510bfc8a1933c6b12730702e2f97
des-ant <57375593+des-ant@users.noreply.github.com>
2022-01-02 11:54:44 +1100

    Declare a new interface that includes the description attribute and extends the CoursePartBase interface

--- part9d-exercises/src/types.ts
+++ part9d-exercises/src/types.ts
@@ -2,3 +2,29 @@ export interface Course {
   name: string;
   exerciseCount: number;
 }
+
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CourseDescription {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CourseDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
